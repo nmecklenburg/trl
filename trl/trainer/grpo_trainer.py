dcb98da269d13bd1137da33d51fe68c9dbd849a0
Nick Mecklenburg <nmecklenburg@berkeley.edu>
2025-06-03 14:14:01 -0700

    uncomment bottom_p arg

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -668,7 +668,7 @@ class GRPOTrainer(Trainer):
                 top_p=self.top_p,
                 top_k=self.top_k,
                 min_p=self.min_p,
-                # bottom_p=self.bottom_p,
+                bottom_p=self.bottom_p,
                 repetition_penalty=self.repetition_penalty,
                 cache_implementation=args.cache_implementation,
             )
@@ -963,7 +963,7 @@ class GRPOTrainer(Trainer):
                         top_p=self.top_p,
                         top_k=-1 if self.top_k is None else self.top_k,
                         min_p=0.0 if self.min_p is None else self.min_p,
-                        # bottom_p=self.bottom_p,
+                        bottom_p=self.bottom_p,
                         max_tokens=self.max_completion_length,
                         guided_decoding_regex=self.guided_decoding_regex,
                     )
